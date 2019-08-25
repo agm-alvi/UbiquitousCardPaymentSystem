f192bfe3b1352609512494dbc25f07ec5ae9b49a
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2019-08-25 17:54:16 +0600

    Update Ubiquitous_Card_System.ino

--- Ubiquitous_Card_System.ino
+++ Ubiquitous_Card_System.ino
@@ -1,8 +1,8 @@
-#include<LiquidCrystal.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <LiquidCrystal.h>
 #include <Keypad.h>
 #include<stdlib.h>
-const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
-LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //three columns
 
@@ -15,28 +15,33 @@ char keys[ROWS][COLS] = {
 
 byte rowPins[ROWS] = {23, 25, 27, 29}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {31, 33, 35, 37}; //connect to the column pinouts of the keypad
-int sum = 0;
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-//void please_pay(char amount[]);
-
-//char amount[5] = {0,0,0,0,0};
+#define SS_PIN 53
+#define RST_PIN 49
+const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
+String lcdprnt = "Paid ";
+int a_amount = 5000, b_amount = 5000, c_amount = 5000;
+int sum = 0;
+char A_card = "3F AD 4D 29";
+char B_card = "BO 77 BB 25";
+char C_card = "71 F3 3D 08";
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Instance of the class
 void setup() {
-  // put your setup code here, to run once:
-  lcd.begin(16,2);
   
-  lcd.print("Welcome");
+  lcd.begin(16,2);
+   Serial.begin(9600);
+   SPI.begin();       // Init SPI bus
+   mfrc522.PCD_Init(); // Init MFRC522
+   lcd.print("Welcome");
   lcd.setCursor(0,1);
   lcd.print("Enter Amount");
   Serial.begin(9600);
   delay(2000);
  
-}
-
-void loop() {
- 
-  // put your main code here, to run repeatedly:
-  
+   //lcd.clear();
+pinMode(8,OUTPUT);//5 volt
 Start:
   lcd.clear();
  //please_pay();
@@ -48,7 +53,7 @@ lcd.print("Key Input");
      key = keypad.getKey();
   if (key != NO_KEY){
    // Serial.println(key);
-    lcd.print(key);
+    Serial.print(key);
     int am;
        switch(key){
       case '0' : am = 0; break;
@@ -63,26 +68,123 @@ lcd.print("Key Input");
       case '9' : am = 9; break;
       
       case 'A' : goto Start; break;
-      case 'C' : sum = 0; break;
+     // case 'B' : sum = (sum-am)/100; am =0; break;
+      
+      case 'C' : sum = 0; am = 0; break;
       case 'D' : goto ending; break;
       
       default :   break;
     }
  
     
-    Serial.print(am);
+    lcd.print(am);
     sum = (sum*10)+ am;
     }
   }
-  ending:
-  Serial.print("Sum =  ");
+ending:
+Serial.println();
+Serial.print("Sum =  ");
 Serial.print(sum);
-delay(2000);
+
 lcd.clear();
-lcd.print("Please Pay:");
-lcd.setCursor(0,1);
+lcd.print("Pay:");
 lcd.print(sum);
+lcd.print(" Taka");
 
+lcd.setCursor(0,1);
 
-exit(0); 
- }
+lcd.println("Swipe Card");
+delay(2000);   
+}
+void loop() {
+  
+
+/*if ( mfrc522.PICC_IsNewCardPresent())
+    {
+        if ( mfrc522.PICC_ReadCardSerial())
+        {
+           Serial.print("Tag UID:");
+           for (byte i = 0; i < mfrc522.uid.size; i++) {
+                  Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+                  Serial.print(mfrc522.uid.uidByte[i], HEX);
+            }
+          
+            Serial.println();
+            mfrc522.PICC_HaltA();
+        }
+}*/ 
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    digitalWrite(8,HIGH);
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  lcd.clear();
+  digitalWrite(8,LOW);
+  Serial.print("UID tag :");
+  lcd.print("UID tag :");
+  delay(2000);
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "3F AD 4D 29") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    a_amount -=sum;
+    Serial.println(a_amount);
+  lcd.clear();
+  lcd.print(content);
+  lcd.setCursor(0,1);
+  lcdprnt = lcdprnt+sum;
+  lcd.print(lcdprnt);
+    delay(2000);
+setup(); 
+  }
+  else if (content.substring(1) == "B0 77 BB 25" ) //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    b_amount -=sum;
+    Serial.println(b_amount);
+  lcd.clear();
+  lcd.print(content);
+  lcd.setCursor(0,1);
+  lcdprnt = lcdprnt+sum;
+  lcd.print(lcdprnt);
+    delay(2000);
+setup();
+}
+ else if (content.substring(1) == "71 F3 3D 08") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    c_amount -=sum;
+    Serial.println(c_amount);
+  lcd.clear();
+  lcd.print(content);
+  lcd.setCursor(0,1);
+  lcdprnt = lcdprnt+sum;
+  lcd.print(lcdprnt);
+    delay(2000);
+setup();
+  }
+ else   {
+    Serial.println(" Access denied");
+    }
+
+}
+ 
