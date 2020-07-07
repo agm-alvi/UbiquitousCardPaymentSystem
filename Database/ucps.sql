5e2f4c05ecda1e646fb28cb6456fbfed56a41a0a
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2020-07-08 04:05:53 +0600

    Update ucps.sql

--- Database/ucps.sql
+++ Database/ucps.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 07, 2020 at 08:36 PM
+-- Generation Time: Jul 08, 2020 at 12:04 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
