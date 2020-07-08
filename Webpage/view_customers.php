9e498b9991f912fb4321ac4ef6324a288077fa2a
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2020-07-08 17:30:20 +0600

    Update view_customers.php

--- Webpage/view_customers.php
+++ Webpage/view_customers.php
@@ -1,110 +1,84 @@
 <?php
-//Creates new record as per request
-    //Connect to database
-    $servername = "localhost";		//example = localhost or 192.168.0.0
-    $username = "root";		//example = root
-    $password = "";	
-    $dbname = "ucps";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Database Connection failed: " . $conn->connect_error);
-    }
-
-
-//fetching data in descending order (lastest entry first)
-
+include 'connection.php';
 
 $result = "SELECT U.u_id, U.Name, U.Balance FROM customers U ORDER BY U.Name ASC";
 $result = mysqli_query($conn, $result);
 
 ?>
+    <!DOCTYPE html>
+    <html>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Users</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="stylesheet" href="listViewStyle.css">
-    <style>
-        table {
-            counter-reset: section;
+    <head>
+        <title>Customers List || UCPS</title>
+        <link rel="icon" type="image/png" href="img/titleIcon.png">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta property="og:image" content="img/UCPS.png" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+        <link rel="stylesheet" href="listViewStyle.css">
+        <style>
+            table {
+                counter-reset: section;
             }
+            
+            .count:before {
+                counter-increment: section;
+                content: counter(section);
+            }
+        </style>
+    </head>
 
-        .count:before {
-            counter-increment: section;
-            content: counter(section);
-        }
-    </style>
-</head>
-<body>
-
-    <div class= "container-fluid" id="header">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="logo"><img src="" alt="LOGO">   </div>  
-            </div>
-            <div class="col-md-8">
-                <div class="menu">
-                    <ul>
-                        <li><a href="index">Home</a></li>
-                        <li><a href="About us">About Us</a></li>
-                        <li><a href="Contact">Contact</a></li>
-                    </ul>
+    <body>
+        <?php include 'Header.php'; ?>
+            <div class="container-fluid" id="top">
+                <div class="row">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-6">
+                        <h1>Customers Informations</h1>
+                        <hr> </div>
+                    <div class="col-md-3"></div>
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-3"></div>
-            
-            <div class="col-md-6">
-                <h1>Users Informations</h1>
-                <hr>
-            </div>
-
-            <div class="col-md-3"></div>
-        </div>
-    </div>
-    
-    <div class="container-fluid" id="tab">
-            <div class="row">
-            <div class="col-md-3"></div>
-            
-            <div class="col-md-6">
-                <table class="table table-hover">
-                    <thead> <tr>
-                        <th>Sl</th>
-                        <th>Name</th>
-                        <th>User Id</th>
-                        <th>Balance</th>
-                    </tr> </thead>
-                    <?php 
+            <div class="container-fluid" id="tab">
+                <div class="row">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-6">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Sl</th>
+                                    <th>Name</th>
+                                    <th>User Id</th>
+                                    <th>Balance</th>
+                                </tr>
+                            </thead>
+                            <?php 
                     while($res = mysqli_fetch_array($result)) { 
                     ?>
-                    <tbody><tr> 
-                        
-                        <td class="count"></td>
-                        <td><?php echo $res['Name']; ?></td>
-                        <td><?php echo $res['u_id']; ?></td>
-                        <td><?php echo $res['Balance'];?></td>
-                    </tr> </tbody> 
-
-                    <?php   
+                                <tbody>
+                                    <tr>
+                                        <td class="count"></td>
+                                        <td>
+                                            <?php echo $res['Name']; ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $res['u_id']; ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $res['Balance'];?>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                                <?php   
                     }
                     ?>
-
-                </table>
-
+                        </table>
+                    </div>
+                    <div class="col-md-3"></div>
+                </div>
             </div>
+            <?php include 'Footer.php'; ?>
+    </body>
 
-            <div class="col-md-3"></div>
-        </div>
-
-    </div>
-
-
-</body>
-</html>
+    </html>
