cee0ae46fb4b24c255af46988a068fa679f17d16
EKHTEAR <30451743+EKHTEAR@users.noreply.github.com>
2020-07-10 04:30:51 +0600

    Update insert_parking-lot.php

--- Webpage/insert_parking-lot.php
+++ Webpage/insert_parking-lot.php
@@ -57,7 +57,7 @@ include 'connection.php';
          $bal = $bal - $amountValue;
         $u_name = $res['Name']; 
         
-        $trx_id = "PL".$id."-".$res['u_sl']."-".$timestamp;//id for Toll Booth (TB)
+        $trx_id = "PL".$id."-".$res['u_sl']."-".$timestamp;//id for Parking Lot (PL)
         echo $res['Balance'];
         echo $bal;
         echo $trx_id;
