85ff54b9ffa9fdf89c09f18ea371fe011ccbc024
EKHTEAR <30451743+EKHTEAR@users.noreply.github.com>
2020-07-10 04:34:05 +0600

    Update insert_card-recharge.php

--- Webpage/insert_card-recharge.php
+++ Webpage/insert_card-recharge.php
@@ -45,7 +45,7 @@ include 'connection.php';
     //  $u_name = $_POST['u_name'];
         $amount = $_POST['amount'];
         $amountValue = (int)$amount;
-        $trx_field = "Card Recharge ". $id;//tollbooth
+        $trx_field = "Card Recharge ". $id;//CardRecharge
         
         
         $result = "SELECT U.u_sl, U.u_id, U.Name, U.Balance FROM customers U WHERE U.u_id = '$u_id'";
@@ -57,13 +57,13 @@ include 'connection.php';
          $bal = $bal + $amountValue;
         $u_name = $res['Name']; 
         
-        $trx_id = "CR".$id."-".$res['u_sl']."-".$timestamp;//id for Toll Booth (TB)
+        $trx_id = "CR".$id."-".$res['u_sl']."-".$timestamp;//id for Card Recharge (CR)
         echo $res['Balance'];
         echo $bal;
         echo $trx_id;
         $sqlUp = "UPDATE `customers` SET `Balance`='".$bal."' WHERE u_id='".$u_id."'";
         
-        $sql1 = "INSERT INTO card_recharge (ID, u_id, u_Name, Amount, Date, Time, trx_id) VALUES ('".$id."','".$u_id."','".$u_name."','".$amountValue."', '".$d."', '".$t."', '".$trx_id."')"; //insert on toll booth table
+        $sql1 = "INSERT INTO card_recharge (ID, u_id, u_Name, Amount, Date, Time, trx_id) VALUES ('".$id."','".$u_id."','".$u_name."','".$amountValue."', '".$d."', '".$t."', '".$trx_id."')"; //insert on Card Recharge table
         
         $sql2 = "INSERT INTO transactions (u_id, u_Name, Amount, trx_field, Date, Time, trx_id) VALUES ('".$u_id."','".$u_name."','".$amountValue."','".$trx_field."', '".$d."', '".$t."', '".$trx_id."')"; //insert on transaction table
         echo "insert";
