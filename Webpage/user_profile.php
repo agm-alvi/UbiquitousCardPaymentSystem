e8edb5813e34c1470cc5187bd4a9f9fe434135b5
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2020-07-21 02:06:06 +0600

    Update user_profile.php

--- Webpage/user_profile.php
+++ Webpage/user_profile.php
@@ -6,170 +6,188 @@ if(empty($_SESSION["username"]))
 }
 require 'connection.php';
 include 'connection.php';
-$name = $_SESSION["username"];
-$resultu = "SELECT * FROM customers U where U.username = '".$name."' ";
+$username = $_SESSION["username"];
+$resultu = "SELECT * FROM customers U where U.username = '".$username."' ";
 $resultu = mysqli_query($conn, $resultu);
 
-$result = "SELECT * FROM transactions T, customers U where T.u_id = U.u_id && U.username = '".$name."'";
+$result = "SELECT * FROM transactions T, customers U where T.u_id = U.u_id && U.username = '".$username."'";
 $result = mysqli_query($conn, $result);
 
 
-$resultr = "SELECT * FROM card_recharge C, customers U where C.u_id = U.u_id && U.username = '".$name."'";
+$resultr = "SELECT * FROM card_recharge C, customers U where C.u_id = U.u_id && U.username = '".$username."'";
 $resultr = mysqli_query($conn, $resultr);
 
 
 ?>
-    <!DOCTYPE html>
-    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-    <head>
-        <title>User Profile || UCPS</title>
-        <meta charset="utf-8">
-        <link rel="icon" type="image/png" href="img/titleIcon.png">
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/profilePageStyle.css"> </head>
+<head>
+    <title>User Profile || UCPS</title>
+    <meta charset="utf-8">
+    <link rel="icon" type="image/png" href="img/titleIcon.png">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/profilePageStyle.css">
+</head>
 
-    <body>
-        <?php include 'Header.php'; ?>
-            <?php  $resu = mysqli_fetch_array($resultu); ?>
-                <div class="container" id="top">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3"></div>
-                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <center>
-                                        <h1>Welcome <?php echo $resu['Name'];?>
+<body>
+    <?php include 'Header.php'; ?>
+    <?php  $resu = mysqli_fetch_array($resultu); ?>
+    <div class="container" id="top">
+        <div class="row">
+            <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3"></div>
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <center>
+                            <h1>Welcome <?php echo $resu['name'];?>
                             </h1>
-                                        <h4>Your Current Balance: <?php echo $resu['Balance'];?></h4> </center>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3"></div>
+                            <h4>Your Current Balance: <?php echo $resu['Balance'];?></h4> <a href="edit_profile.php">Edit Profile</a>
+                        </center>
                     </div>
                 </div>
-                <div class="container-fluid" id="tab">
-                    <div class="row">
-                        <nav class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
-                            <ul class="nav nav-tabs">
-                                <li class="active"><a data-toggle="tab" href="#Transactions">Transactions</a></li>
-                                <li><a data-toggle="tab" href="#Recharges">Recharges</a></li>
-                            </ul>
-                        </nav>
-                        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
-                            <div class="tab-content">
-                                <div id="Transactions" class="tab-pane fade in active">
-                                    <h2>Transactions</h2>
-                                    <table class="table table-hover">
-                                        <thead>
-                                            <tr>
-                                                <th>Sl</th>
-                                                <th>Name</th>
-                                                <th>User Id</th>
-                                                <th>Amount</th>
-                                                <th>Sector</th>
-                                                <th>Date</th>
-                                                <th>Time</th>
-                                                <th>Transaction Id</th>
-                                            </tr>
-                                        </thead>
-                                        <?php 
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3"></div>
+        </div>
+    </div>
+    <div class="container-fluid" id="tab">
+        <div class="row">
+            <nav class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+                <ul class="nav nav-pills flex-column">
+                    <li class="active"><a data-toggle="tab" href="#Profile">Profile</a></li>
+                    <li><a data-toggle="tab" href="#Transactions">Transactions</a></li>
+                    <li><a data-toggle="tab" href="#Recharges">Recharges</a></li>
+                </ul>
+            </nav>
+            <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+                <div class="tab-content">
+                    <div id="Profile" class="tab-pane fade in active">
+                         <?php 
+                   // while($resi = mysqli_fetch_array($resultu)) { ?>
+                        
+                        <strong>Username: <?php echo $resu['username']; ?></strong>
+                        <strong>User ID:  <?php echo $resu['u_id']; ?>    </strong>
+                        <strong>Contact no: <?php echo $resu['contact_no']; ?></strong>
+                        <strong>Email: <?php echo $resu['email'];?></strong>
+                        <strong>House & Street no: <?php echo $resu['hNs_no']; ?></strong>
+                        <strong>Thana: <?php echo $resu['thana']; ?></strong>
+                        <strong>District: <?php echo $resu['district'];?></strong>
+                        <strong>Zip Code: <?php echo $resu['zip_code'];?></strong>
+                        <strong>Country: <?php echo $resu['country'];?></strong>        
+                    <?php   //}                    ?>
+                    </div>
+                    <div id="Transactions" class="tab-pane fade">
+                        <h2>Transactions</h2>
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Sl</th>
+                                    <th>Name</th>
+                                    <th>User Id</th>
+                                    <th>Amount</th>
+                                    <th>Sector</th>
+                                    <th>Date</th>
+                                    <th>Time</th>
+                                    <th>Transaction Id</th>
+                                </tr>
+                            </thead>
+                            <?php 
                     while($res = mysqli_fetch_array($result)) { 
                     ?>
-                                            <tbody>
-                                                <tr>
-                                                    <td>
-                                                        <?php echo $res['sl']; ?>
-                                                    </td>
-                                                    <td>
-                                                        <?php echo $res['u_Name']; ?>
-                                                    </td>
-                                                    <td>
-                                                        <?php echo $res['u_id']; ?>
-                                                    </td>
-                                                    <td>
-                                                        <?php echo $res['Amount'];?>
-                                                    </td>
-                                                    <td>
-                                                        <?php echo $res['trx_field']; ?>
-                                                    </td>
-                                                    <td>
-                                                        <?php echo $res['Date']; ?>
-                                                    </td>
-                                                    <td>
-                                                        <?php echo $res['Time'];?>
-                                                    </td>
-                                                    <td>
-                                                        <?php echo $res['trx_id'];?>
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-                                            <?php   
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <?php echo $res['sl']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $res['u_Name']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $res['u_id']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $res['Amount'];?>
+                                    </td>
+                                    <td>
+                                        <?php echo $res['trx_field']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $res['Date']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $res['Time'];?>
+                                    </td>
+                                    <td>
+                                        <?php echo $res['trx_id'];?>
+                                    </td>
+                                </tr>
+                            </tbody>
+                            <?php   
                     }
                     ?>
-                                    </table>
-                                </div>
-                                <div id="Recharges" class="tab-pane fade">
-                                    <h2>Recharges</h2>
-                                    <table class="table table-hover">
-                                        <thead>
-                                            <tr>
-                                                <th>Sl</th>
-                                                <th>Name</th>
-                                                <th>User Id</th>
-                                                <th>Amount</th>
-                                                <th>Sector</th>
-                                                <th>Date</th>
-                                                <th>Time</th>
-                                                <th>Recharge Id</th>
-                                            </tr>
-                                        </thead>
-                                        <?php 
+                        </table>
+                    </div>
+                    <div id="Recharges" class="tab-pane fade">
+                        <h2>Recharges</h2>
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Sl</th>
+                                    <th>Name</th>
+                                    <th>User Id</th>
+                                    <th>Amount</th>
+                                    <th>Sector</th>
+                                    <th>Date</th>
+                                    <th>Time</th>
+                                    <th>Recharge Id</th>
+                                </tr>
+                            </thead>
+                            <?php 
                     while($res = mysqli_fetch_array($resultr)) { 
                     ?>
-                                            <tbody>
-                                                <tr>
-                                                    <td>
-                                                        <?php echo $res['sl']; ?>
-                                                    </td>
-                                                    <td>
-                                                        <?php echo $res['u_Name']; ?>
-                                                    </td>
-                                                    <td>
-                                                        <?php echo $res['u_id']; ?>
-                                                    </td>
-                                                    <td>
-                                                        <?php echo $res['Amount'];?>
-                                                    </td>
-                                                    <td> </td>
-                                                    <td>
-                                                        <?php echo $res['Date']; ?>
-                                                    </td>
-                                                    <td>
-                                                        <?php echo $res['Time'];?>
-                                                    </td>
-                                                    <td>
-                                                        <?php echo $res['trx_id'];?>
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-                                            <?php   
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <?php echo $res['sl']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $res['u_Name']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $res['u_id']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $res['Amount'];?>
+                                    </td>
+                                    <td> </td>
+                                    <td>
+                                        <?php echo $res['Date']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $res['Time'];?>
+                                    </td>
+                                    <td>
+                                        <?php echo $res['trx_id'];?>
+                                    </td>
+                                </tr>
+                            </tbody>
+                            <?php   
                     }
                     ?>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2"></div>
+                        </table>
                     </div>
                 </div>
-                <?php include 'Footer.php'; ?>
-    </body>
+            </div>
+            <div class="col-xs-12 col-sm-0 col-md-1 col-lg-1"></div>
+        </div>
+    </div>
+    <?php include 'Footer.php'; ?>
+</body>
 
-    </html>
+</html>
