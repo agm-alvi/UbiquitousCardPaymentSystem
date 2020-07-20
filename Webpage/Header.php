4158714eda65fc1245f15637b2907ccbba3b5591
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2020-07-21 02:06:19 +0600

    Update Header.php

--- Webpage/Header.php
+++ Webpage/Header.php
@@ -14,7 +14,7 @@ if(!isset($_SESSION))
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/indexPageStyle.css">
+    <link rel="stylesheet" href="css/HeaderFooterStyle.css">
 </head>
 
 <body>
@@ -30,7 +30,7 @@ if(!isset($_SESSION))
                         <li><a href="index.php">Home</a></li>
                         <li><a href="index.php#about_us">About Us</a></li>
                         <li><a href="index.php#portfolio">Fields</a></li>
-                        
+
                         <?php 
                         if(empty($_SESSION["username"])){?>
                         <li class="dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="#">Sign In <span class="caret"></span></a>
@@ -41,7 +41,14 @@ if(!isset($_SESSION))
                         </li>
                         <?php }
                          else{?>
-                        <li><a href="logout.php">Sign out</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <?php echo $_SESSION["username"] ?><span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="logout.php">Sign Out</a></li>
+                            </ul>
+                        </li>
                         <?php } ?>
                         <li><a href="Contact.php">Contact Us</a></li>
                     </ul>
