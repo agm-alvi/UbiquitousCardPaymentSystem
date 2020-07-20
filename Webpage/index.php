bf090153e3e21cc68f2d02c6c8ecce33609dfbf8
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2020-07-21 02:06:12 +0600

    Update index.php

--- Webpage/index.php
+++ Webpage/index.php
@@ -14,6 +14,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/indexPageStyle.css">
+    <link rel="stylesheet" href="css/HeaderFooterStyle.css">
 </head>
 
 <body>
