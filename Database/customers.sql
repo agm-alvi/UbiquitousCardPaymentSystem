46211c051b1fe649fc1e0628b56e66f4e97abac0
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2020-07-23 04:19:05 +0600

    DB

--- Database/customers.sql
+++ Database/customers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 20, 2020 at 09:30 PM
+-- Generation Time: Jul 22, 2020 at 11:58 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
@@ -37,6 +37,7 @@ CREATE TABLE `customers` (
   `password` varchar(50) NOT NULL,
   `contact_no` varchar(15) NOT NULL,
   `email` varchar(30) NOT NULL,
+  `gender` varchar(8) NOT NULL,
   `hNs_no` varchar(200) NOT NULL,
   `thana` varchar(20) NOT NULL,
   `district` varchar(20) NOT NULL,
@@ -48,13 +49,14 @@ CREATE TABLE `customers` (
 -- Dumping data for table `customers`
 --
 
-INSERT INTO `customers` (`u_sl`, `u_id`, `name`, `Balance`, `username`, `password`, `contact_no`, `email`, `hNs_no`, `thana`, `district`, `zip_code`, `country`) VALUES
-('001', '3F AD 4D 29', 'Jahid Hasan Alvi', 5500, 'alvi', 'abcd', '01822933773', 'jahidhasanalvi007@gmail.com', '15/1 Faridabad Lane,  Besides Faridabad High School', 'Gendaria', 'Dhaka', 1204, 'Bangladesh'),
-('002', 'B0 77 BB 25', 'Ekhtear Uddin Khan', 4700, 'ekhtear', 'efgh', '0', '', '', '', '', 0, ''),
-('003', 'A1 F4 78 D5', 'Asha Das', 4800, 'asha', 'ijkl', '0', '', '', '', '', 0, ''),
-('004', 'C1 7B E5 D5', 'UVW', 5000, '', '', '0', '', '', '', '', 0, ''),
-('005', '71 F3 3D 08', 'XYZ', 1700, '', '', '0', '', '', '', '', 0, ''),
-('006', '41 24 9D D5', 'PQR', 4300, '', '', '0', '', '', '', '', 0, '');
+INSERT INTO `customers` (`u_sl`, `u_id`, `name`, `Balance`, `username`, `password`, `contact_no`, `email`, `gender`, `hNs_no`, `thana`, `district`, `zip_code`, `country`) VALUES
+('000', '000', 'admin', 0, 'admin', 'admin', '00000000000', 'null', '', 'null', 'null', 'null', 0, 'null'),
+('001', '3F AD 4D 29', 'Jahid Hasan Alvi', 5500, 'alvi', 'abcd', '01822933773', 'jahidhasanalvi007@gmail.com', 'Male', '15/1 Faridabad Lane,  Besides Faridabad High School', 'Gendaria', 'Dhaka', 1204, 'Bangladesh'),
+('002', 'B0 77 BB 25', 'Ekhtear Uddin Khan', 4700, 'ekhtear', 'efgh', '0', '', 'Male', '', '', '', 0, ''),
+('003', 'A1 F4 78 D5', 'Asha Das', 4800, 'asha', 'ijkl', '0', '', 'Female', '', '', '', 0, ''),
+('005', '71 F3 3D 08', 'Auni', 1700, 'auni', 'nta', '0', '', 'Female', '', '', '', 0, ''),
+('006', '41 24 9D D5', 'PQR', 4300, '', '', '0', '', '', '', '', '', 0, ''),
+('010', 'C1 7B E5 D5', 'Tanjila Farah', 5000, 'tnf', 'tnf', '0', '', 'Female', '', '', '', 0, '');
 
 --
 -- Indexes for dumped tables
