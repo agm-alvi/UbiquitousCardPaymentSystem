d1614610bcf5388660abab1f787aa8ddaa517756
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2021-02-24 16:37:56 +0600

    Update insert_card-recharge.php

--- Webpage/insert_card-recharge.php
+++ Webpage/insert_card-recharge.php
@@ -22,7 +22,7 @@ $amount = 0;
 
     <head>
         <meta charset="UTF-8">
-        <title>Card Recharge Bill Pay || UCPS</title>
+        <title>Card Recharge || UCPS</title>
         <link rel="icon" type="image/png" href="img/titleIcon.png">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -36,65 +36,42 @@ $amount = 0;
 
     <body>
         <?php include 'Header.php'; ?>
-            <div class="container">
+            <div class="container inputform">
                 <div class="row">
                     <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></div>
                     <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
-                        <h1>Card Recharge Fare Pay</h1>
+                        <h1>Recharge Center</h1>
                         <hr>
-                        <form action="insert_card-recharge.php" method="post" name="CRForm" id="CRForm">
+                        <form action="insert_toll-booth.php" method="post" name="TBForm" id="TBForm">
                             <div class="form-group">
-                                <h3>Card Recharge ID:
+                                <h3>Recharge Center ID:
                             <input type="text" name="u_id" value="<?php
                                 echo $wid?>" disabled></h3> </div>
                             <div class="form-group">
                                 <h3>User ID:
                             <input type="text" name="u_id" placeholder="Enter User ID" required></h3> </div>
-                            <div class="form-group">
-                                <h3><label for="vType">Vehicle:</label>
-                            <select class="vType" id="vType" name="vType">
-                                <option value="none" selected disabled <?php if (isset($vType) && $vType=="null") echo "selected";?>>--Select--</option>
-                                <option value="privateCar" <?php if (isset($vType) && $vType=="privateCar") echo "selected";?>>Private Car</option>
-                                <option value="micro" <?php if (isset($vType) && $vType=="micro") echo "selected";?>>Micro Bus</option>
-                                <option value="bus" <?php if (isset($vType) && $vType=="bus") echo "selected";?>>Bus</option>
-                                <option value="cng" <?php if (isset($vType) && $vType=="cng") echo "selected";?>>CNG</option>
-                                <option value="motorCycle" <?php if (isset($vType) && $vType=="motorCycle") echo "selected";?>>Motor Cycle</option>
-                            </select></h3> </div>
-                            <?php
-                            
-                            if($vType=="privateCar"){
-                                $amount = 100;
-                            }
-                            else if($vType=="micro"){
-                                $amount = 150;
-                            }
-                            else if($vType=="bus"){
-                                $amount = 300;
-                            }
-                            else if($vType=="cng"){
-                            $amount = 50;
-                            }
-                            else if($vType=="motorCycle"){
-                            $amount = 10;
-                            }
-                            else{
-                                $amount = 0;
-                            }
-                                
-                            ?>
+                           
+                           
                                 <div class="form-group">
                                     <h3>Amount:
-                            <input type="text" name="amount" placeholder="null" value="<?php
-                                echo $amount?>" disabled></h3> </div>
+                            <input type="text" name="amount" placeholder="Enter Amount" ></h3> </div>
                                 <button type="submit" class="btn btn-dark" style="font-weight:900;" value="submit">Submit</button>
                         </form>
                     </div>
-                </div> <a href="login_vendor.php">Go Back</a> </div>
+                </div> <a class="btn2" href="login_vendor.php">Go Back</a> </div>
             <?php
-                                          echo $vType;
+        
+                                    
         ///*
 include 'connection.php';
-    //Get current date and time
+function random_color_part(){
+    return str_pad(dechex(mt_rand(0,255)),2,'0', STR_PAD_LEFT);
+}
+function rand_hex(){
+    return random_color_part().random_color_part();
+}
+                                                                 
+//Get current date and time
     date_default_timezone_set('Asia/Dhaka');
     $d = date("Y-m-d");
     $t = date("H:i:s");
@@ -108,27 +85,29 @@ include 'connection.php';
 	//	$u_name = $_POST['u_name'];
 	//	$amount = $_POST['amount'];
         $amountValue = (int)$amount;
-        $trx_field = "Card Recharge ". $wid;//cardrecharge
+        $trx_field = "Recharge Center". $wid;//tollbooth
         
         
-        $result = "SELECT U.u_sl, U.u_id, U.Name, U.Balance FROM customers U WHERE U.u_id = '$u_id'";
+        $result = "SELECT U.u_sl, U.uAccountNo, U.Balance FROM customers U WHERE U.u_id = '$u_id'";
         $result = mysqli_query($conn, $result);
 
         $res = mysqli_fetch_array($result);
         
         $bal = (int)$res['Balance'] ;
-         $bal = $bal - $amountValue;
+         $bal = $bal + $amountValue;
         $u_name = $res['Name']; 
         
-        $trx_id = "CR".$wid."-".$res['u_sl']."-".$timestamp;//id for Card Recharge (CR)
+       // $trx_id = "RC".$wid."-".$res['u_sl']."-".$timestamp;//id for Toll Booth (TB)
+        $trx_id = rand_hex().str_pad(dechex($idValue),3,'0', STR_PAD_LEFT).substr($platform, -1).rand_hex();
+        
         echo $res['Balance'];
         echo $bal;
         echo $trx_id;
-        $sqlUp = "UPDATE `customers` SET `Balance`='".$bal."' WHERE u_id='".$u_id."'";
+        $sqlUp = "UPDATE `customers` SET `Balance`='".$bal."' WHERE uAccountNo='".$u_id."'";
         
-		$sql1 = "INSERT INTO card_recharge (ID, u_id, u_Name, Amount, Date, Time, trx_id) VALUES ('".$wid."','".$u_id."','".$u_name."','".$amountValue."', '".$d."', '".$t."', '".$trx_id."')"; //insert on Card Recharge table
+		$sql1 = "INSERT INTO toll_booth (ID, uAccountNo, Amount, Date, Time, trx_id) VALUES ('".$wid."','".$u_id."','".$amountValue."', '".$d."', '".$t."', '".$trx_id."')"; //insert on toll booth table
         
-        $sql2 = "INSERT INTO transactions (u_id, u_Name, Amount, trx_field, Date, Time, trx_id) VALUES ('".$u_id."','".$u_name."','".$amountValue."','".$trx_field."', '".$d."', '".$t."', '".$trx_id."')"; //insert on transaction table
+        $sql2 = "INSERT INTO transactions (u_id, u_Name, Amount, trx_field, Date, Time, trx_id) VALUES ('".$u_id."','".$amountValue."','".$trx_field."', '".$d."', '".$t."', '".$trx_id."')"; //insert on transaction table
         echo "insert";
 		if ($conn->query($sql1) === TRUE&&$conn->query($sql2) === TRUE&&$conn->query($sqlUp) === TRUE) {
 		    echo "OK";
