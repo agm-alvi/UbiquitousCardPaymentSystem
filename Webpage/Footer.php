c38d54ebd8e741377fe9144cd100a7a4920daf92
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2021-02-25 03:46:17 +0600

    Update Footer.php

--- Webpage/Footer.php
+++ Webpage/Footer.php
@@ -8,25 +8,40 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/HeaderFooterStyle.css"> </head>
+    <link rel="stylesheet" href="css/HeaderFooterStyle.css">
+    <style>
+    .footer .sites ul {
+    margin-left: -45px;
+}
+    </style>
+</head>
 
 <body>
 
     <!-- Footer Container Starts -->
     <div class="container-fluid footer">
         <div class="row">
-            <p>Copyright 2020 UCPS - Built For Better Future</p>
+            <div class="col-md-3"></div>
+            <div class="col-md-6">
+                <p style="text-align: center;">Copyright 2020 UCPS - Built For Better Future</p>
+            </div>
+            <div class="col-md-3"></div>
         </div>
-        <div class="row">
+        
             <div class="sites">
-                <ul>
-                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                    <li><a href="#"><i class="fa fa-telegram"></i></a></li>
-                    <li><a href="#"><i class="fa fa-snapchat-ghost"></i></a></li>
-                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                </ul>
+        <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-8">
+                    <ul>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fa fa-telegram"></i></a></li>
+                        <li><a href="#"><i class="fa fa-snapchat-ghost"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2"></div>
             </div>
         </div>
     </div>
