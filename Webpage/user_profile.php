1a4588a4b257ba4772df0faab016d3f71b535d6f
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2021-02-25 08:33:25 +0600

    Update user_profile.php

--- Webpage/user_profile.php
+++ Webpage/user_profile.php
@@ -34,6 +34,12 @@ $resultr = mysqli_query($conn, $resultr);
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/profilePageStyle.css">
     <style>
+        body {
+            background-image: url("img/profileBG.jpg");
+            background-color: #115511;
+             background-size: 100%;
+        }
+
         table {
             counter-reset: section;
         }
